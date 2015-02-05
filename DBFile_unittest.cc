35f683bb33e177aad375e62c825a64c865e298eb
Rui Zhang <ruizhang1011@ufl.edu>
2015-02-05 13:16:05 -0500

    fixed gtest using fixture and separate test table dir to global

--- DBFile_unittest.cc
+++ DBFile_unittest.cc
@@ -1,23 +1,25 @@
 #include <limits.h>
 #include "DBFile.h"
+#include "Schema.h"
+#include "Record.h"
 #include "gtest/gtest.h"
 #include "stdio.h"
+
+char tbl_dir[256] = "/cise/tmp/dbi_sp11/DATA/10M/lineitem.tbl"; 
+char tbl_name[50] = "lineitem";
+char catalog_path[100] = "source/catalog";
+
 
 class DBFileTest : public ::testing::Test {
 	protected:
 	virtual void SetUp() {
-		dbfile.Create("testFile.bin",heap,0);
-		f_schema = ("source/catalog", "lineitem");
-		strcpy(tbl_dir, "/cise/tmp/dbi_sp11/DATA/10M/lineitem.tbl");
+		dbfile.Create("testFile.bin", heap , 0);
 	}
 
 	virtual void TearDown() {
 		dbfile.Close();
 	}
-	DBFile dbfile;	
-	Schema f_schema;
-	char tbl_dir[256]; 
-	Record tempRec1, tempRec2;
+	DBFile dbfile;		
 };
 
 TEST_F(DBFileTest, CreateFile) {
@@ -32,17 +34,14 @@ TEST_F(DBFileTest, OpenFile) {
 }
 
 TEST_F(DBFileTest, CloseFile) {
-	int f_flag = dbfile.Close();
-	EXPECT_EQ(1 , f_flag);   //close created file
-	dbfile.Open("testFile.bin");
-	f_flag = dbfile.Close(); //close open file
-	EXPECT_EQ(1 , f_flag);	
-	dbfile.Create("testFile.bin",heap,0);	//avoid close twice, may be not necessary
+ 	EXPECT_EQ(1 , dbfile.Close() );   //close created file
+	dbfile.Open("testFile.bin"); 
+	EXPECT_EQ(1 , dbfile.Close() );	//close open file
 }
 
 TEST_F(DBFileTest, CreateHeapTypeHeader) {
 	char type[8];
-	FILE * fp = fopen("testFile.bin.header","r");
+	FILE * fp = fopen("testFile.header","r");
 	ASSERT_TRUE(fp != NULL);
 	fscanf(fp,"%s",type);
 	fclose(fp);		
@@ -50,9 +49,10 @@ TEST_F(DBFileTest, CreateHeapTypeHeader) {
 }
 
 TEST_F(DBFileTest, LoadFile) {
+	Schema f_schema(catalog_path, tbl_name);
 	EXPECT_EQ( 0, dbfile.curFile.GetLength() );
 	dbfile.Load (f_schema, tbl_dir);
-	EXPECT_GE( 2, dbfile.curFile.GetLength() );
+	EXPECT_LE( 2, dbfile.curFile.GetLength() );
 }
 
 TEST_F(DBFileTest, MoveFirst) {
@@ -61,6 +61,8 @@ TEST_F(DBFileTest, MoveFirst) {
 
 TEST_F(DBFileTest, AddRecord) {
 	int i;
+	Record tempRec1;
+	Schema f_schema(catalog_path, tbl_name);
 	FILE *tableFile = fopen (tbl_dir, "r");
 	ASSERT_TRUE(tableFile != NULL);
 	
@@ -72,14 +74,15 @@ TEST_F(DBFileTest, AddRecord) {
 	
 	for(i = 0; i<800; i++){
 		tempRec1.SuckNextRecord (&f_schema, tableFile);	
-		dbfile.Add (tempRec);
+		dbfile.Add (tempRec1);
 	}
 	EXPECT_EQ( 3, dbfile.curFile.GetLength() );	
 	fclose(tableFile);
 }
 
 TEST_F(DBFileTest, GetNext) {
-	int i;
+	Record tempRec1, tempRec2;
+	Schema f_schema(catalog_path, tbl_name);
 	FILE *tableFile = fopen (tbl_dir, "r");
 	ASSERT_TRUE(tableFile != NULL);
 	
@@ -87,7 +90,7 @@ TEST_F(DBFileTest, GetNext) {
 	dbfile.MoveFirst();
 	
 	while ( dbfile.GetNext(tempRec2) && tempRec1.SuckNextRecord (&f_schema, tableFile) ){
-		EXPECT_EQ( 0, strcmp( tempRec1.bits, tempRec2.bits );
+		EXPECT_EQ( 0, strcmp( tempRec1.GetBits(), tempRec2.GetBits() ) );
 	}
 	fclose(tableFile);
 }
@@ -96,6 +99,8 @@ TEST(DBFileDeathTest, OpenNonExistFile) {
 	DBFile dbfile;
 	EXPECT_DEATH(dbfile.Open("nonexist.bin"), "BAD!  Open did not work for nonexist.bin");	
 }
+
+
 
 
 
