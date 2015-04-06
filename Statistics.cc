66569c0329ec1c03b490aece1eaf845b27ec6add
RayZ-O <rayzhang1011@gmail.com>
2015-04-06 00:50:11 -0400

    Cost estimation draft

--- Statistics.cc
+++ Statistics.cc
@@ -4,116 +4,254 @@ Statistics::Statistics () {
 	relations.reserve(8);
 }
 
-Statistics::Statistics (Statistics &copyMe) {
+Statistics::Statistics (Statistics const &copyMe) {
 	relations = copyMe.relations;
 }
 
-// typedef unordered_map<string,double> Attributes;
-// typedef pair<double, Attributes> Info;
-// typedef unordered_map<string, Info> Relations;
-
-void Statistics::AddRel (char *relName, int numTuples) {
+void Statistics::AddRel (char *relName, long numTuples) {
 	// if the relation name equal to NULL
 	if (nullptr == relName) {
 		cerr << "ERROR: Invalid relation name in AddRel\n";
-		exit(1);
+		std::terminate();
 	}
-	// if the relation is not in statisics
-	if (relations.find(relName) == relations.end()) {
-		// add this relation in
-		Attributes emptyAtts;	
-		Info info = make_pair(numTuples, emptyAtts);
-		relations.emplace(relName, info);
-	} else {
-		// otherwise upadte number of tuples of this relation
-		get<0>(relations.at(relName)) = numTuples;
-	}	
+	// add this relation in
+	auto p = relations.emplace(relName, RelInfo(numTuples, relName));
+	// if the relation is in statisics, upadte number of tuples of this relation
+	if (false == p.second) {
+		//p.first = Relations::iterator
+		p.first->second.numTuples = numTuples;		
+	} 
 }
 
-void Statistics::AddAtt (char *relName, char *attName, int numDistincts) {
+void Statistics::AddAtt (char *relName, char *attName, long numDistincts) {
 	//if the names are invalid, report and exit
 	if (nullptr == relName || nullptr == attName) {
 		cerr << "ERROR: Invalid relation name or attribute name in AddAtt\n";
-		exit(1);
+		std::terminate();
 	}
 	// if the relation is not in statisics, report error and exit
 	if (relations.find(relName) == relations.end()) {
 		cerr << "ERROR: Attempt to add attribute to non-exist relation!\n";
-		exit(1);
+		std::terminate();
 	} 
 	// obtain attribute list of this relation
-	auto &atts = get<1>(relations.at(relName));
-	// if this attribute is not in attribute list
-	if (atts.find(attName) == atts.end()){
-		// add it into attribute list
-		atts.emplace(attName, numDistincts);
-	} else {
-		// otherwise update number of distinct values of this attribute
-		atts.at(attName) = numDistincts;		
+	auto &atts = relations.at(relName).atts;
+	auto p = atts.emplace(attName, numDistincts);
+	// if the attribute is in statisics, upadte number of distinct value of this attribute
+	if (false == p.second) {
+		//p.first = AttInfo::iterator
+		p.first->second = numDistincts;		
 	}
 }
 
 void Statistics::CopyRel (char *oldName, char *newName) {
+	//Add into relations with new name
+	auto p = relations.emplace(newName, RelInfo(relations.at(oldName)));
 	// if the old relation is not in statisics, report error and exit
-	if (relations.find(oldName) == relations.end()) {
+	if (false == p.second) {
 		cerr << "ERROR: Attempt to copy non-exist relation!\n";
-		exit(1);
-	} else {
-		//copy old relation infomation
-		Info info = make_pair(get<0>(relations.at(oldName)), get<1>(relations.at(oldName)));
-		//Add into relations with new name
-		relations.emplace(newName, info);
+		std::terminate();
 	}	
 }
 	
 void Statistics::Read (const char *fromWhere) {
-	ifstream infile(fromWhere);
+	std::ifstream infile(fromWhere);
 	if (!infile.is_open()) {
 		return;
 	}
-	string relName;
-	string attName;
-	double numTuples, numDistincts;
-	Attributes atts;
+	std::string relName;
+	std::string attName;
+	long numTuples, numDistincts;
+	AttInfo atts;
 	atts.reserve(16);
 	while (infile.good()) {
 		infile >> relName;
-		infile >> numTuples;		
+		infile >> numTuples;
+		RelInfo info(numTuples, relName);	
 		while (1) {
 			infile >> attName;
 			if ("REL_END" == attName) {
 				break;
 			}
 			infile >> numDistincts;
-			atts.emplace(attName, numDistincts);
+			info.atts.emplace(attName, numDistincts);
 		}
-		Info info = make_pair(numTuples, atts);
-		relations.emplace(relName, info);
+		relations.emplace(relName, std::move(info));
 		atts.clear();
 	}
 }
 
 void Statistics::Write (const char *fromWhere) {
-	ofstream outfile(fromWhere);
+	std::ofstream outfile(fromWhere);
 	if (!outfile.is_open()) {
-		cerr << "ERROR: Can not oper output file in Statistics::Write\n";
-		exit(1);
+		cerr << "ERROR: Can not open output file in Statistics::Write\n";
+		std::terminate();
 	}
-	for (auto rel : relations) {
-		outfile << rel.first << " " << get<0>(rel.second) << endl;
-		for (auto atts : get<1>(rel.second)) {
-			outfile << atts.first << " " << atts.second << endl;
+	for (auto const &rel : relations) {
+		outfile << rel.first << " " << rel.second.numTuples << endl;
+		for (auto att : rel.second.atts) {
+			outfile << att.first << " " << att.second << endl;
 		}
 		outfile << "REL_END" <<endl;
 		outfile << endl;
 	}
 }
 
-void  Statistics::Apply (struct AndList *parseTree, char *relNames[], int numToJoin) {
+void Statistics::Apply (const struct AndList *parseTree, char *relNames[], int numToJoin) {
+	
+
 
 }
 
-double Statistics::Estimate (struct AndList *parseTree, char **relNames, int numToJoin) {
+long Statistics::Estimate (const struct AndList *parseTree, char **relNames, int numToJoin) {
 
 }
 
+long Statistics::Simulate (const struct AndList *parseTree, char **relNames, int numToJoin) {
+	std::unordered_set<std::string> relToJoin;
+	for (int i = 0; i < numToJoin; i++) {
+		//if the relations to join is not in current statistics, report error and exit
+		if (relations.find(relNames[i]) == relations.end()) {
+			cerr << "ERROR: Current statistics does not contain " << relNames << endl;
+			std::terminate();
+		}
+		//otherwis hash them into a hash table relToJoin
+		relToJoin.emplace(relNames[i]);
+	}
+	int numSet = 0;
+	//while there is elements in the hash table
+	while (relToJoin.size() > 0) {
+		auto rel = relToJoin.begin();
+		//get the subset list of first element in the table
+		std::vector<std::string> &subset = relations.find(*rel)->second.subset;
+		//erase this element in the hash table after get its subset member
+		relToJoin.erase(rel);
+		//for each elements in the subset 
+		for (auto const &srel : subset) {
+			//if one of the elements in the subset is not in relations to join, this join does not make sense
+			//because all relation in the subset list have been joined into one relation
+			if (relToJoin.find(srel) == relToJoin.end()) {
+				cerr << "ERROR: Attempt to join incomplete subset members\n";
+				std::terminate();
+			} else {
+				//erase the matched relation
+				relToJoin.erase(rel);
+			}
+		}
+		++numSet;
+	}
+	if (1 == numSet) {
+		if (nullptr == parseTree) {
+			// do noting
+		} else {
+			// seletion
+		}		
+	} else {
+		if (nullptr == parseTree) {
+			// cross product
+		} else {
+			// (join | seletion)*
+		}
+		
+	}
+}
+
+// #define LESS_THAN 1
+// #define GREATER_THAN 2
+// #define EQUALS 3
+
+// // these are the types of operands that can appear in a CNF expression
+// #define DOUBLE 1
+// #define INT 2
+// #define STRING 3
+// #define NAME 4
+
+// struct Operand {
+// 	int code;
+// 	char *value;
+// };
+// struct ComparisonOp {
+// 	int code;
+// 	struct Operand *left;
+// 	struct Operand *right;	
+// };
+
+void Statistics::EstimateOrList(struct AndList *pAnd) {
+	while (pAnd != nullptr) {
+		struct OrList *pOr = pAnd->left;
+		std::unordered_map<std::string, float> seletivities;
+		while (pOr != nullptr) {
+			struct ComparisonOp *pCom = pOr->left;
+			int op = pCom->code;
+			int lOperand = pCom->left->code;
+			int rOperand = pCom->right->code;
+			// NAME = NAME
+			if (EQUALS == op && NAME == lOperand | rOperand) {
+				std::string lname(pCom->left->value);
+				std::string rname(pCom->right->value);
+				auto lrel = FindAtts(lname);
+				auto rrel = FindAtts(rname);
+				seletivities.emplace(std::make_pair(lrel.fisrt, 1.0));
+				seletivities.emplace(std::make_pair(rrel.fisrt, 1.0));
+			}
+			// NAME , value | value , name
+			else if (lOperand | rOperand > 4) {
+				if (lOperand == NAME) {
+					std::string name(pCom->left->value);				
+				} else {
+					std::string name(pCom->right->value);
+				}
+				auto rel = FindAtts(name);
+				AttInfo &atts = rel.second.atts;
+				float seletivity =  EQUALS == op ? 1 / atts.at(name) : 1.0 / 3;
+				auto p = seletivities.emplace(std::make_pair(rel.fisrt, seletivity));
+				if (false == p.second) {
+					float s = p.first->second;
+					//seletivity
+					p.first->second = s == 1 ? selectivity : 1 - (1 - seletivity) * (1 - s);
+				}
+			}
+			pOr = pOr->rightOr;
+		}
+		for (auto s : seletivities) {
+			//pair<iterator, bool>
+			auto ibp = sizeParameters.emplace(std::make_pair(s.first, s.second));
+			if (false == ibp.second) {
+				//selectivity multiplication
+				ibp.first->second *= s.second;
+			}
+		}
+		pAnd = pAnd->rightAnd
+	}
+	
+}
+
+const Relations& Statistics::FindAtts(std::string &name) {
+	for (auto const &rel : relations) {
+		const AttInfo &atts = rel.second.atts;
+		if (atts.end() != atts.find(name)) {
+			return rel;
+		}
+	}
+	cerr << "ERROR: Attempt to join relations using non-exist attribute!\n";
+	std::terminate();
+}
+
+long Statistics::SimulateJoin(struct ComparisonOp *pCom) {
+
+}
+
+long Statistics::SimulateSeletion(struct ComparisonOp *pCom) {
+
+}
+
+void Statistics::Print () const {
+	for (auto const &rel : relations) {
+		cout << "Relation name: " << rel.first << " | num of tuples: " << rel.second.numTuples << endl;
+		cout << "Attribute list: " << endl;
+		for (auto const &att : rel.second.atts) {
+			cout << "Attribute name: " << att.first << " | num of distinct: " << att.second << endl;
+		}
+		cout << endl;
+	}
+}
--- Statistics.h
+++ Statistics.h
@@ -1,47 +1,67 @@
 #ifndef STATISTICS_
 #define STATISTICS_
 #include "ParseTree.h"
-#include <stdlib.h>
+#include <exception>
 #include <iostream>
 #include <fstream>
-#include <vector>
 #include <unordered_map>
+#include <unordered_set>
 #include <utility>
 #include <string>
+#include <vector>
 
+using std::cout;
 using std::cerr;
-using std::vector;
-using std::unordered_map;
-using std::pair;
-using std::make_pair;
-using std::string;
-using std::get;
-using std::ofstream;
-using std::ifstream;
 using std::endl;
 
-typedef unordered_map<string,double> Attributes;
-typedef pair<double, Attributes> Info;
-typedef unordered_map<string, Info> Relations;
+// attribute name, number of distinct value
+typedef std::unordered_map<std::string,long> AttInfo;
+// number of tuples, attribute list
+typedef struct info{
+	long numTuples;
+	AttInfo atts;
+	std::vector<std::string> subset;
+	info(long num, std::string name) : numTuples(num), atts(), subset{name} { }
+	info(info && rhs) : numTuples(rhs.numTuples) {
+		atts = std::move(rhs.atts);
+		subset = std::move(rhs.subset);
+	}
+	info(const info& rhs) : numTuples(rhs.numTuples), atts(rhs.atts), subset(rhs.subset) { }
+}RelInfo;
+// relation name, relation info: number of tuples, attribute list
+typedef std::unordered_map<std::string, RelInfo> Relations;
+
+// typedef struct einfo{
+// 	float selectivity; 
+// 	AttInfo atts;	
+// }EstInfo;
 
 class Statistics
 {
 private:
 	Relations relations;
+	std::unordered_map<std::string, float> sizeParameters;
+	void EstimateAndList(struct AndList *pAnd);
+	long SimulateJoin(struct ComparisonOp *pCom);
+	long SimulateSeletion(struct ComparisonOp *pCom);
+	long Simulate (const struct AndList *parseTree, char **relNames, int numToJoin);
+	const Relations& FindAtts(std::string &name);
+
 public:
 	Statistics();
-	Statistics(Statistics &copyMe);	 // Performs deep copy
+	Statistics(Statistics const &copyMe);	 // Performs deep copy
 
-	void AddRel(char *relName, int numTuples);
-	void AddAtt(char *relName, char *attName, int numDistincts);
+	void AddRel(char *relName, long numTuples);
+	void AddAtt(char *relName, char *attName, long numDistincts);
 	void CopyRel(char *oldName, char *newName);
 	
 	void Read(const char *fromWhere);
 	void Write(const char *fromWhere);
 
-	void  Apply(struct AndList *parseTree, char *relNames[], int numToJoin);
-	double Estimate(struct AndList *parseTree, char **relNames, int numToJoin);
-
+	void Apply(const struct AndList *parseTree, char *relNames[], int numToJoin);
+	long Estimate(const struct AndList *parseTree, char **relNames, int numToJoin);
+	
+	void Print() const;
 };
 
 #endif
